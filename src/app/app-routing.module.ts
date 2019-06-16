5604b8271bf346fe194798334f7419118ae337a0
K.A.D.Chathurangani <dinuarachchige14@gmail.com>
2019-06-17 00:42:28 +0530

    customer form created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboadComponent } from './component/dashboad/dashboad.component';
+import { ManageCustomerComponent } from './component/manage-customer/manage-customer.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'dashboad',
+    component: DashboadComponent
+  },
+  {
+    path: 'manage-customer',
+    component: ManageCustomerComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboad'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
