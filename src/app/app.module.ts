05e8ac0cd4e65a8c42ff3f44c6bd23da6d865550
K.A.D.Chathurangani <dinuarachchige14@gmail.com>
2019-06-17 12:10:36 +0530

    customer dto created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
+import { DashboadComponent } from './component/dashboad/dashboad.component';
+import { ManageCustomerComponent } from './component/manage-customer/manage-customer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DashboadComponent,
+    ManageCustomerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
