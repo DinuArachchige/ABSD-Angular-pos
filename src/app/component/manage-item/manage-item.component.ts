e38dae5232e426a631b984f6e69165de7806c13a
K.A.D.Chathurangani <dinuarachchige14@gmail.com>
2019-06-19 13:13:09 +0530

    update item coding

--- src/app/component/manage-item/manage-item.component.ts
+++ src/app/component/manage-item/manage-item.component.ts
@@ -14,7 +14,7 @@ export class ManageItemComponent implements OnInit {
   selectedItems: Item = new Item('', '', '', '');
   tempItems: Item = null;
   manuallySelected = false;
-  @ViewChild('frmItems') frmItems: NgForm;
+  @ViewChild('frmItem') frmItem: NgForm;
 
   constructor(private itemService: ItemService) { }
 
@@ -22,25 +22,13 @@ export class ManageItemComponent implements OnInit {
     this.loadAllItems();
   }
 
-   loadAllItems() {
-    this.itemService.getAllItems().subscribe((result) => {
-      this.items = result;
-      console.log(this.items);
-    });
-  }
 
-  selectItems(item: Item) {
-    this.itemService.searchItem(item.code).subscribe((value) => {
-      if (value) {
-        this.selectedItems = value;
-      }
-    });
-  }
-
-  saveItems() {
-    this.itemService.saveItem(this.selectedItems).subscribe((result) => {
+  saveItems(): void {
+    this.itemService.saveItem(this.selectedItems)
+      .subscribe((result) => {
       if (result) {
         alert('Item has been saved successfully');
+        this.items.push(this.selectedItems);
         this.loadAllItems();
       } else {
         alert('Failed to save the item');
@@ -48,20 +36,57 @@ export class ManageItemComponent implements OnInit {
     });
   }
 
-  deleteItems(code) {
-    if (confirm('Are you sure, you want to delete this item?'))  {
-      this.itemService.deleteItem(code).subscribe((result) => {
-        if (result) {
-          alert('Item has been deleted successfully');
-        } else {
-          alert('Fail to delete Item');
+   loadAllItems(): void {
+     this.itemService.getAllItems().subscribe(
+       (result) => {
+         this.items = result;
+         console.log(this.items);
+       }
+     );
+  }
+
+  deleteItems(code): void {
+    if (confirm('Are you sure you want to delete this Item?')) {
+
+      this.itemService.deleteItem(code).subscribe(
+        (result) => {
+          if (result) {
+            alert('Items has been Deleted successfully');
+          } else {
+            alert('Failed to deleted Items');
+          }
+          this.loadAllItems();
         }
-      });
+      );
     }
   }
 
-  clear() {
+  update(id): void {
+    this.itemService.saveItem(this.selectedItems).subscribe(
+      value => {
+        if (value) {
+          alert('Items has been Update successfully');
+          this.loadAllItems();
+        } else {
+          alert('Failed to update the Items');
+        }
+      }
+    );
+  }
+
+  selectItems(item: Item): void {
+    this.itemService.searchItem(item.code).subscribe(
+      value => {
+        if (value) {
+          this.selectedItems = value;
+        }
+      }
+    );
+  }
+
+  clear(): void {
     const index = this.items.indexOf(this.selectedItems);
+
     if (index !== -1) {
       this.items[index] = this.tempItems;
       this.tempItems = null;
@@ -70,14 +95,4 @@ export class ManageItemComponent implements OnInit {
     this.manuallySelected = false;
   }
 
-  update(code) {
-    this.itemService.saveItem(this.selectedItems).subscribe((value) => {
-      if (value) {
-        alert('Item has been update successfully');
-        this.loadAllItems();
-      } else {
-        alert('failed to update item');
-      }
-    });
-  }
 }
